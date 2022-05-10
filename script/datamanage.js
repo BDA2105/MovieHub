fab5e933c24ea0157663394e78b63f52ba8fa588
arimaz1881 <zama1689@gmail.com>
2022-05-10 09:36:31 +0600

    added profile page

---
+++ script/datamanage.js
@@ -0,0 +1,19 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://arimaz:123@cluster0.hqvwy.mongodb.net/movieHub?retryWrites=true&w=majority";
+
+function record() {
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    let dbo = db.db("movieHub");
+        let myobj = {
+            name: document.getElementById('name').value,
+            nickname: document.getElementById('nickname').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value
+        };
+        dbo.collection("users").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+});}
